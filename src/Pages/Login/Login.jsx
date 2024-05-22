7e4317b969985c58235769484fc87af3724dac88
1OmarTarek1 <cr7omartarek@gmail.com>
2024-05-22 19:11:24 +0300

    FINISH LOGIN

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { MDBInput, MDBBtn } from 'mdb-react-ui-kit';
 import { useNavigate } from 'react-router-dom';
 import './Login.css'
+import { FaFacebookSquare, FaGoogle } from 'react-icons/fa';
+import BackVid from '../../Assets/Videos/144584-785095786_medium.mp4'
 
 
 
@@ -14,40 +16,73 @@ const Login = ({setAuthenticated}) => {
     const [passValue, setPassValue] = useState('');
 
     const handleLogin = () => {
-        // Perform login logic...
         setAuthenticated(true);
-        // Redirect to the Profile page after successful login
         navigate("/");
     };
 
 
     return (
         <>
-            <div className="mainForm Login-Page">
-                <form className="loginForm" >
+            <div className="mainFormWrapper Login-Page">
+                <div className="video-background">
+                    <video className='backVid' src={BackVid} autoPlay loop muted />
+                </div>
+                <form className="mainForm loginForm" >
+                    <div className="formTitle">
+                        Login
+                    </div>
+
                     <MDBInput
                     value={emailValue}
                     onChange={(e) => setEmailValue(e.target.value)}
                     label='Email Address'
+                    className='mainInput'
                     id='emailLogin'
                     type='email'
+                    size="lg" 
                     />
 
                     <MDBInput
                     value={passValue}
                     onChange={(e) => setPassValue(e.target.value)}
                     label='Password'
+                    className='mainInput'
                     id='passLogin'
                     type='password'
+                    size="lg" 
                     />
 
+                    <div className="formItem">
+                        <div>
+                            <input type="checkbox" name="" id="saveCheck"/>
+                            <label htmlFor="saveCheck" style={{marginLeft:"5px"}}>Save Password</label>
+                        </div>
+                        <span className=''>Forget Password ?</span>
+                    </div>
+
                     <MDBBtn 
-                    className='' 
-                    tag='input' 
+                    className='mainButton' 
+                    tag='button' 
                     type='submit' 
                     value='Login' 
+                    size="lg"
                     onClick={handleLogin}
-                    />
+                    > Login </MDBBtn>
+
+                    <div className='d-flex justify-content-center gap-2'>
+                        <div className="register formItem">
+                            Don't have an account ? Register
+                        </div>
+                        <div className="logWith d-flex justify-content-center gap-1">
+                            <div className="withFacebook">
+                                <FaFacebookSquare />
+                            </div>
+                            -
+                            <div className="withGmail">
+                                <FaGoogle />
+                            </div>
+                        </div>
+                    </div>
                 </form>
             </div>
         </>
