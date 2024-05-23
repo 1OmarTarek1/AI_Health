ea76d3a8d1356bc3a7ff7228d741952f7e518b77
1OmarTarek1 <cr7omartarek@gmail.com>
2024-05-23 12:08:07 +0300

    edit category page

--- src/Components/Cards/CategoryCard/CategoryCard.jsx
+++ src/Components/Cards/CategoryCard/CategoryCard.jsx
@@ -1,74 +1,67 @@
-import { FaFireAlt } from "react-icons/fa";
-import { FaDumbbell, FaRadiation, FaShieldHalved } from 'react-icons/fa6';
+// import { FaFireAlt } from "react-icons/fa";
+// import { FaDumbbell, FaRadiation, FaShieldHalved } from 'react-icons/fa6';
+import { FaFileLines } from 'react-icons/fa6';
 import './CategoryCard.css'
 
+const CategoryCard = (props) => {
 
-
-
-
-
-const CategoryCard = ( props ) => {
+    const handleClick = () => {
+        if (props.onClick) {
+            props.onClick(); // Call the onClick function if it's provided
+        }
+    };
 
     return (
-        <>
-            <div className="CategoryCard">
-                <div className="cardHeader">
-                    <div className="cardImgWrapper">
-                        <img src={props.imageUrl} alt={"props.name"} className="cardImg" loading="lazy" />
-                    </div>
-                    <div className="cardName">
-                        { props.title }
-                    </div>
+        <div className="CategoryCard"> {/* Add onClick handler here */}
+            <div className="cardHeader">
+                <div className="cardImgWrapper">
+                    <img src={props.imageUrl} alt={"props.name"} className="cardImg" loading="lazy" />
                 </div>
-                <div className="cardBody">
-                    <p>
-                        { props.title }{ props.title }{ props.title }
-                        { props.title }{ props.title }{ props.title }
-                        { props.title }{ props.title }{ props.title }
-                        { props.title }{ props.title }{ props.title }
-                    </p>
-                    <ul className="foodDetails">
-                        <li className="detailItem">
-                            <div className="miniTitle">
-                                <FaFireAlt style={{color:"Red"}} />
-                                <span>Calories</span>
-                            </div>
-                            <div className="titleInfo">
-                                {props.calories}
-                            </div>
-                        </li>
-                        <li className="detailItem">
-                            <div className="miniTitle">
-                                <FaDumbbell style={{color:"silver"}} />
-                                <span>Protein</span>
-                            </div>
-                            <div className="titleInfo">
-                                {props.protein}
-                            </div>
-                        </li>
-                        <li className="detailItem">
-                            <div className="miniTitle">
-                                <FaRadiation style={{color:"yellow"}} /> 
-                                <span>Fats</span>
-                            </div>
-                            <div className="titleInfo">
-                                {props.fats}
-                            </div>
-                        </li>
-                        <li className="detailItem">
-                            <div className="miniTitle">
-                                <FaShieldHalved style={{color:"lightGreen"}}/>
-                                <span>Healthy</span>
-                            </div>
-                            <div className="titleInfo">
-                                {props.healOpj}
-                            </div>
-                        </li>
-                    </ul>
+                <div className="cardName">
+                    {props.title}
                 </div>
+                <button className="moreBtn" onClick={handleClick}>
+                    <FaFileLines />
+                </button>
             </div>
-        </>
+            <div className="cardBody">
+                <ul className="foodDetails">
+                    <li className="detailItem">
+                        <div className="miniTitle">
+                            <span>Calories</span>
+                        </div>
+                        <div className="titleInfo">
+                            {props.calories}
+                        </div>
+                    </li>
+                    <li className="detailItem">
+                        <div className="miniTitle">
+                            <span>Protein</span>
+                        </div>
+                        <div className="titleInfo">
+                            {props.protein}
+                        </div>
+                    </li>
+                    <li className="detailItem">
+                        <div className="miniTitle">
+                            <span>Fats</span>
+                        </div>
+                        <div className="titleInfo">
+                            {props.fats}
+                        </div>
+                    </li>
+                    <li className="detailItem">
+                        <div className="miniTitle">
+                            <span>Healthy</span>
+                        </div>
+                        <div className="titleInfo">
+                            {props.healOpj}
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
-export default CategoryCard
+export default CategoryCard;
